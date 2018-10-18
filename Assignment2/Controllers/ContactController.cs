3f370fc892fc5073b3943bce5ebf257c488210e2
Levi Hayes <levi.hayes@oit.edu>
2018-10-18 00:49:12 -0700

    Upload Assignment2 Final upload?

---
+++ Assignment2/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new ContactModel());
+        }
+        [HttpPost]
+        public IActionResult Create(ContactModel contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", contact);
+            }
+            return View("Create", contact);
+        }
+    }
+}
---
+++ Assignment2/Models/ContactModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment2.Models
+{
+    public class ContactModel
+    {
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "The name is required")]
+        public string name { get; set; }
+
+        [DisplayName("Phone Number")]
+        [Required(ErrorMessage = "The phone number is required")]
+        public string phoneNumber { get; set; }
+
+        [DisplayName("Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string email { get; set; }
+
+        [DisplayName("Message")]
+        [Required(ErrorMessage = "The message is required")]
+        public string message { get; set; }
+
+    }
+}
